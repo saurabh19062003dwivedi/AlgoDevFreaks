9db3a0a3e98c6b8eb84125359465306c85f36db0
saurabh19062003dwivedi <saurabhdwivedi2003sta@gmail.com>
2025-05-11 17:22:53 +0530

    Normalize line endings

---
+++ server/models/Profile.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const ProfileSchema = new mongoose.Schema({
+    gender:{
+        type:String,
+
+    },
+    dateOfBirth:{
+        type:String,
+    },
+    about:{
+        type:String,
+        trim:true,
+
+    },
+    connectNumber:{
+        type:Number,
+        trim:true,
+    },
+    profileimage: { type: String },
+});
+
+module.exports = mongoose.model("Profile", ProfileSchema);
